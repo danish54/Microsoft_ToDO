c3883e7607bfb5ebc23144b9f13e19ec3b5aa4d8
Danish Ali <v-danishali@microsoft.com>
2022-08-24 19:33:27 +0530

    MS TODO

--- Microsoft_ToDO/UnitTest1.cs
+++ Microsoft_ToDO/UnitTest1.cs
@@ -1,3 +1,6 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
 namespace Microsoft_ToDO
 {
     [TestClass]
@@ -6,6 +9,16 @@ namespace Microsoft_ToDO
         [TestMethod]
         public void TestMethod1()
         {
+            //String user_name = "";
+            //String Pass = "";
+            String Base_url = "https://to-do.microsoft.com";
+
+            IWebDriver driver;
+            driver = new EdgeDriver(); 
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Navigate().GoToUrl(Base_url);
         }
     }
 }
